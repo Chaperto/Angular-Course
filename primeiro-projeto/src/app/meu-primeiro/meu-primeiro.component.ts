e208c30a9dfdf63db2d47034d7ac0a7182c3e32f
Felipe Nascimento Rocha <df82614es@df.trf1.gov.br>
2018-08-29 17:08:05 -0300

    firstcommit

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+ 
+@Component({
+    selector: "meu-primeiro-component",
+    template: `
+        <p>Meu Primeiro Component com Angular!</p>
+`
+})
+  export class MeuPrimeiroComponent {
+
+
+
+}
